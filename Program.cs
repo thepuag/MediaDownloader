a889484b2e2955b2b086d9be5f391ba0dbd6f250
Noe <noe.m.garc@gmail.com>
2025-05-01 22:58:12 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Uso: downloader <URL de YouTube>");
+            return;
+        }
+        var url = args[0];
+        var youtube = new YoutubeClient();
+
+        // Obtener manifest de streams (vídeo+audio)
+        var manifest = await youtube.Videos.Streams.GetManifestAsync(url);
+        // Seleccionar el mejor stream combinado (muxed)
+        var streamInfo = manifest.GetMuxedStreams()
+                                 .GetWithHighestVideoQuality();
+
+        var fileName = $"{streamInfo.VideoId}.{streamInfo.Container}";
+        Console.WriteLine($"Descargando {fileName}...");
+
+        // Descargar al directorio actual
+        await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName);
+        Console.WriteLine("Descarga completada.");
+    }
+}
